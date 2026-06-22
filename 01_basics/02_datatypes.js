808639039568deb886c3f0842cc3c10df04c786f
Trushant Rathod <trushantrathod0206@gmail.com>
2026-06-22 07:29:55 +0000

    2-datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) //we are using node.js not browser
+
+// console.log(3+3); console.log("Trushant")  //correctbut not readable
+
+console.log(3+3)
+console.log("Trushant") //now readable
+
+let name = "Trushant" //string
+let age = 20; //int
+let isLogged = true //bool
+let state = null //null
+let country; //undefined
+
+//number / integer => 2 to power 53
+//bigint => bigger than integer range
+//string => ""
+//boolean => true / false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof("trushant")) //to identify types
+
+console.log(typeof age) //to identify types of already defined above
+
+console.log(typeof null) // output: object
+console.log(typeof undefined) // output: undefined
