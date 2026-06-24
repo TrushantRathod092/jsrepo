509aa45eb35d3d3bd87e0066184625f4e1b02e78
Trushant Rathod <trushantrathod0206@gmail.com>
2026-06-24 07:19:41 +0000

    stack and heap memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -7,7 +7,7 @@ const anotherId = Symbol('123')
 
 // although we have give dame id's but they will be different because of symbol datatype which is used for uniqueness
 
-console.log(id == anotherId) // output: false
+// console.log(id == anotherId) // output: false
 
 
 // Reference / Non- Primitive
@@ -24,3 +24,25 @@ let myobj = {
 const myFunction = function(){
     console.log("Hello World")
 }
+
+
+//Stack (Primitive) , Heap(Non-Primitive)
+
+let myName = "Trushant"
+let anotherName = myName
+anotherName = "Bittu"
+
+// console.log(myName)
+// console.log(anotherName)
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "trushant@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
