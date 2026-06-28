590bf3f058fb729b7692d7211d5997bb2e98d940
Trushant Rathod <trushantrathod0206@gmail.com>
2026-06-28 12:06:18 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let i = 1
+// while (i <= 10) {
+//     console.log(i);
+//     i += 2 // i = i + 2
+// }
+
+let myArray = ["batman", "spiderman", "superman"]
+// let i = 0
+
+// while (i < myArray.length) {
+//     console.log(`${i} -> element is ${myArray[i]}`);
+//     i++
+// }
+
+let score = 1
+do {
+    console.log(score);
+    score++
+} while (score <= 10);
