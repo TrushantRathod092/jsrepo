c91e5f48b8519ed38ae0ff80a06e100e504370a2
Trushant Rathod <trushantrathod0206@gmail.com>
2026-07-02 14:46:37 +0000

    keyboard project

---
+++ 07_Projects/6-Keyboard/chaiaurcode.js
@@ -0,0 +1,19 @@
+const insert = document.querySelector('#insert');
+
+window.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    insert.innerHTML =`
+    <div class="color">
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>Code</th>
+            </tr>
+            <tr>
+                <td>${e.key === ' ' ? 'Space' : e.key}</td>
+                <td>${e.code}</td>
+            </tr>
+        </table>
+    </div>
+    `
+});
